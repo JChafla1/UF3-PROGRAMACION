300c40f31af0f0077f8e8106a81610cd08a64898
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-04-07 17:46:07 +0200

    Eliminar archivo

--- Delete.py
+++ Delete.py
@@ -1,22 +1,20 @@
 import os
 
-# Codigo reciclado del primer ejercico
 class BorrarArchivo:
     def __init__(self):
         # Leer ruta
         ruta = self.leer_ruta()
         # Mirar si existe
         if os.path.isfile(ruta):
-            # Si existe, quitar extension
-            ruta_sin_extension = quitar_extension(ruta)
-            os.rename(ruta, ruta_sin_extension)
+            # Si existe, eliminar
+            os.rename(ruta)
         else:
             print("El fichero no existe")
 
     def leer_ruta(self):
         ruta = input("Escribe la ruta del fichero existente: ")
         return ruta
-    
+
 
 # Instanciar la clase para que se ejecute automáticamente al ser importada
 BorrarArchivo()

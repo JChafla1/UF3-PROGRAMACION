37bb4bc4993e2b80a00ed02f43e51106bd205b50
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-04-07 17:27:01 +0200

    Add files via upload

---
+++ Delete.py
@@ -0,0 +1,21 @@
+import os
+
+class BorrarArchivo:
+    def __init__(self):
+        # Leer ruta
+        ruta = leer_ruta()
+        # Mirar si existe
+        if os.path.isfile(ruta):
+            # Si existe, quitar extension
+            ruta_sin_extension = quitar_extension(ruta)
+            os.rename(ruta, ruta_sin_extension)
+        else:
+            print("El fichero no existe")
+
+    def leer_ruta(self):
+        ruta = input("Escribe la ruta del fichero existente: ")
+        return ruta
+    
+
+# Instanciar la clase para que se ejecute automáticamente al ser importada
+BorrarArchivo()

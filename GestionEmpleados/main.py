c3a6884bf9e81978f3ccd8f09c009475fcabf85e
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-04-14 19:06:45 +0200

    Cuerpo

--- GestionEmpleados/main.py
+++ GestionEmpleados/main.py
@@ -1,1 +1,27 @@
 import gestion_empleados
+
+def main():
+    while True:
+        gestion_empleados.mostrar_menu()
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            # Añadir empleado
+
+
+        elif opcion == "2":
+            # Buscar empleados
+
+
+        elif opcion == "3":
+            # Listar empleados
+
+        elif opcion == "4":
+            print("Adios!")
+            break
+
+        else:
+            print("Opcion no valida. Selecciona una opcion valida.")
+
+if __name__ == "__main__":
+    main()

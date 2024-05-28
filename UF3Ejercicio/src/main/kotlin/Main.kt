d89cf897e4775e785af2b5be93379b4f0cb367d8
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 00:29:04 +0200

    While

--- UF3Ejercicio/src/main/kotlin/Main.kt
+++ UF3Ejercicio/src/main/kotlin/Main.kt
@@ -8,19 +8,12 @@ fun main() {
     val inventory = Inventory()
     val menu = cargarInterfazUsuario("menu.txt")
 
-    // Agregar elementos al inventario
-    inventory.afegirItem(Item(1, "Espada", "Arma", 100, 2.5))
-    inventory.afegirItem(Item(2, "Escudo", "Armadura", 150, 5.0))
-    inventory.afegirItem(Item(3, "Poción de Vida", "Poción", 50, 0.1))
-
-    // Guardar el inventario en un archivo JSON
-    inventory.guardarInventarioAJson("inventario.json")
-
-    // Cargar el inventario desde el archivo JSON
-    inventory.cargarInventarioDesDeJson("inventario.json")
-
-    // Mostrar el inventario cargado
-    println("Inventario cargado:")
-    inventory.obtenerTodosLosItems().forEach { println(it) }
+    while (true) {
+        println(menu)
+        print("Selecciona una opció: ")
+        when (readLine()?.toIntOrNull() ?: 0) {
+            
+        }
+    }
     
 }

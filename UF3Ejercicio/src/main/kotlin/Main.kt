888842a54d722ab88c16640b0dc3fb1b7d24200c
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-28 23:20:46 +0200

    JSON

--- UF3Ejercicio/src/main/kotlin/Main.kt
+++ UF3Ejercicio/src/main/kotlin/Main.kt
@@ -6,4 +6,11 @@ fun main() {
     inventory.afegirItem(Item(1, "Espada", "Arma", 100, 2.5))
     inventory.afegirItem(Item(2, "Escudo", "Armadura", 150, 5.0))
     inventory.afegirItem(Item(3, "Poción de Vida", "Poción", 50, 0.1))
+
+    // Guardar el inventario en un archivo JSON
+    inventory.guardarInventarioAJson("inventario.json")
+
+    // Cargar el inventario desde el archivo JSON
+    inventory.cargarInventarioDesDeJson("inventario.json")
+    
 }

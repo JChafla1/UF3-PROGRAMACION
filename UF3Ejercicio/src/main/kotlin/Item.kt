e5721ec46fe533e8aa80d1e5ff94f9443ff338e5
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-28 22:44:42 +0200

    Item

--- UF3Ejercicio/src/main/kotlin/Item.kt
+++ UF3Ejercicio/src/main/kotlin/Item.kt
@@ -1,3 +1,13 @@
-package org.example
+/*He usado la data class debido al ahorro de codigo que me proporcionaria hacer
+* una class normal, ya que esta clase me proporciona implementaciones por defecto
+* para funciones como "equals()", "hashCode()" y cosas asi */
 
-data class Item()
+data class Item(
+    // Atributos
+    val id: Int,
+    val nom: String,
+    val tipus: String,
+    val valor: Int,
+    val pes: Double
+    
+)

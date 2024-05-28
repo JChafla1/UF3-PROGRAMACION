a1b44c583ae30b7d29ccdbc49c3553fb32cc1f9c
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-28 23:18:33 +0200

    AgregarElementos

--- UF3Ejercicio/src/main/kotlin/Main.kt
+++ UF3Ejercicio/src/main/kotlin/Main.kt
@@ -1,16 +1,9 @@
-package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val inventory = Inventory()
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    // Agregar elementos al inventario
+    inventory.afegirItem(Item(1, "Espada", "Arma", 100, 2.5))
+    inventory.afegirItem(Item(2, "Escudo", "Armadura", 150, 5.0))
+    inventory.afegirItem(Item(3, "Poción de Vida", "Poción", 50, 0.1))
 }

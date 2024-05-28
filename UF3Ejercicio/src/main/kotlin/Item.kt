a9cd1a161793d64f84ded2f678d69380085aca70
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-28 23:08:46 +0200

    Serializable

--- UF3Ejercicio/src/main/kotlin/Item.kt
+++ UF3Ejercicio/src/main/kotlin/Item.kt
@@ -2,6 +2,9 @@
 * una class normal, ya que esta clase me proporciona implementaciones por defecto
 * para funciones como "equals()", "hashCode()" y cosas asi */
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Item(
     // Atributos, los he cambiado a castellano que me lio si no
     val id: Int,

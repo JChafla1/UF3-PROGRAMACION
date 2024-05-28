374762aed3d56c7b8ead0b73623f85264355ddae
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 00:30:36 +0200

    opcion 1

--- UF3Ejercicio/src/main/kotlin/Main.kt
+++ UF3Ejercicio/src/main/kotlin/Main.kt
@@ -12,7 +12,22 @@ fun main() {
         println(menu)
         print("Selecciona una opció: ")
         when (readLine()?.toIntOrNull() ?: 0) {
-            
+            1 -> {
+                println("Introduce los datos del item:")
+                print("ID: ")
+                val id = readLine()?.toIntOrNull() ?: 0
+                print("Nombre: ")
+                val nombre = readLine() ?: ""
+                print("Tipo: ")
+                val Tipo = readLine() ?: ""
+                print("Valor: ")
+                val valor = readLine()?.toIntOrNull() ?: 0
+                print("Peso: ")
+                val peso = readLine()?.toDoubleOrNull() ?: 0.0
+
+                inventory.agregarNuevoItem(id, nombre, Tipo, valor, peso)
+                println("Ítem añadido con exito")
+            }
         }
     }
     
